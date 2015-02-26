06d945e750d094d602a40083d844778b7773b605
charlotte-d <dye22c@mtholyoke.edu>
2015-02-26 10:31:15 -0500

    dsfds

--- src/AnswerPanel.java
+++ src/AnswerPanel.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
