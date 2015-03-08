6e188d415e0d6c4ccc5832dd55bf44599ef997cf
charlotte-d <dye22c@mtholyoke.edu>
2015-03-08 15:57:02 -0400

    Made the kit page and made bark beetle page show up (still need to fix appearance)

--- src/passport/KitPage.java
+++ src/passport/KitPage.java
@@ -1,23 +1,148 @@
 package passport;
 
 import java.awt.Color;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 public class KitPage extends JPanel {
+
+	// The width of the page for the kit
+	private static final int PAGE_WIDTH = 500;
+
+	// The height of the page for the kit
+	private static final int PAGE_HEIGHT = 700;
+
+	// The name of the page
+	private String pageName;
+
+	// The name of the child
+	private String childName;
+
+	// The name/location of the image this page shows:
+	// NOTE: THIS WILL CHANGE TO GET THE APPROPRIATE STICKER
+	// We should either have the image file name be some sort of parameter
+	// or we could have standardized file names (e.g., sticker_[KIT NAME].png)
+	private static final String IMAGE_FILE = "src//images//logo.png";
+
+	// The font for the kit name
+	private static final Font KIT_NAME_FONT = new Font("Times New Roman",
+			Font.BOLD, 72);
+
+	// The font for the line with the child's name
+	private static final Font CHILD_FONT = new Font("Times New Roman",
+			Font.PLAIN, 60);
+
+	// The text color
+	private static final Color TEXT_COLOR = Color.BLACK;
+
+	// The background color
+	private static final Color BACKGROUND_COLOR = Color.YELLOW;
 	
-	private String pageName ;
-	
-	private String childName ;
-	
-	public KitPage(){  
+	//Whether the sticker should be displayed
+	private boolean showSticker;
+
+	/**
+	 * Creates a new page for the kit
+	 * 
+	 * @param pageName
+	 *            The name of the page
+	 *            
+	 * @param showSticker Whether the sticker should be displayed
+	 */
+	public KitPage(String pageName, String childName, boolean showSticker) {
+
+		//Note whether we should show the sticker
+		this.showSticker = showSticker;
 		
-		this.setBackground(Color.yellow);
+		// Set the page name
+		this.pageName = pageName;
+
+		// Save the name of the child
+		this.childName = childName;
+
+		// Set the layout to BoxLayout
+		//this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+		//this.setLayout(new FlowLayout(FlowLayout.CENTER, 50, 25));
+
+		// Set the size
+		this.setSize(new Dimension(PAGE_WIDTH, PAGE_HEIGHT));
+
+		// Set the background color
+		this.setBackground(BACKGROUND_COLOR);
+
+		// Add everything to the page
+		addContent();
+
+	}
+	
+	/**
+	 * Add the content to the kit page
+	 */
+	private void addContent(){
+		
+		//Add the kit name header
+		//The kit name label
+		JTextArea kitNameArea = new JTextArea(pageName,2,7);
+		
+		//Set the font of the kit name label
+		kitNameArea.setFont(KIT_NAME_FONT);
+		
+		//Set the font color
+		kitNameArea.setForeground(TEXT_COLOR);
+		
+		kitNameArea.setWrapStyleWord(true);
+		
+		kitNameArea.setLineWrap(true);
+		
+		kitNameArea.setEditable(false);
+		
+		
+		
+		//Add to the panel
+		add(kitNameArea);
+		
+		//Check if we should add the sticker
+		if (showSticker){
+			//Show the sticker
+			
+			//Create the sticker icon
+			ImageIcon imageIcon = new ImageIcon(IMAGE_FILE);
+			
+			//Create a label for the sticker
+			JLabel stickerLabel = new JLabel(imageIcon);
+			
+			//Add the sticker
+			add(stickerLabel);
+		}
+		else {
+			//Don't show the sticker
+			//Leaving this here because we might want to specifically
+			//add some blank space later
+		}
+		
+		//Add the child's name
+		//Child's name label
+		JLabel childNameLabel = new JLabel(childName);
+		
+		//Set the font of the child's name
+		childNameLabel.setFont(CHILD_FONT);
+		
+		//Set the font color
+		childNameLabel.setForeground(TEXT_COLOR);
+		
+		//Center the child's name
+		childNameLabel.setHorizontalAlignment(JLabel.CENTER);
+		
+		//Add to the page
+		add(childNameLabel);
 		
 	}
-
-	
 
 }
--- src/passport/Passport.java
+++ src/passport/Passport.java
@@ -8,91 +8,146 @@ import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Passport extends JPanel implements MouseListener{
-	
-	private static final int PAGE_WIDTH = 500; 
+public class Passport extends JPanel implements MouseListener {
 
+	// Passport should take in fake name, (maybe grade?)
+	// Every page should take title of kit
+	// Page could need add sticker method
+	// Use logo.png as sticker for now, or get beetle thing
+
+	// The name of the child whose passport this is
+	private String childName;
+
+	// The width of the page of the passport
+	private static final int PAGE_WIDTH = 500;
+
+	// The height of the page of the passport
 	private static final int PAGE_HEIGHT = 700;
-	
+
+	// The layout for the passport
 	private static final CardLayout CARD_LAYOUT = new CardLayout();
 
-	private static final String FIRST_PAGE_NAME = "First Page" ;
-	
-	private static final FirstPage firstPage = new FirstPage() ; 
-	
-	
-	private ArrayList<String> pageNames  = new ArrayList<String>() ;
-	
-	private int currentPage = 0 ; 
-	
-	public Passport(){
-		
-		this.setLayout(CARD_LAYOUT) ;
-		
-		this.addMouseListener(this);
-		
-		addPage("Beetle Page") ;
-		
-		this.add(firstPage, FIRST_PAGE_NAME) ;
-		
-		CARD_LAYOUT.show(this,FIRST_PAGE_NAME) ;
-				
-		
-		
-	}
-	
+	// The name of the first page
+	private static final String FIRST_PAGE_NAME = "First Page";
+
+	// The first page of the passport
+	private FirstPage firstPage;
+	//private static final FirstPage firstPage = new FirstPage();
+
+	// The list of page names
+	private ArrayList<String> pageNames = new ArrayList<String>();
+
+	// The currently displayed page
+	private int currentPage = 0;
+
 	/**
+	 * Create a new passport
+	 * 
+	 * @param childName
+	 *            The name of the child
+	 */
+	public Passport(String childName) {
+		firstPage = new FirstPage(childName);
+		
+		// Save the child's name (will be needed for various pages)
+		this.childName = childName;
+		
+		// Set the layout to the card layout we created
+		this.setLayout(CARD_LAYOUT);
+
+		// Add a mouse listener so that when we click on the passport,
+		// it goes to the next page
+		this.addMouseListener(this);
+
+		//Add the name of the first page
+		pageNames.add(FIRST_PAGE_NAME);
+		
+		// Add the first page to the panel
+		this.add(firstPage, FIRST_PAGE_NAME);
+
+		// Display the first page
+		CARD_LAYOUT.show(this, FIRST_PAGE_NAME);
+
+		// Add the bark beetle page to the passport 
+		//NOTE: At some point we need to find whether we should show the sticker
+		addPage("Bark Beetle Infestation", true);
+
+	}
+
+	/**
+	 * Add the page with the specified name to the list of page names
 	 * 
 	 * @param pageName
+	 *            The name of the page to add
+	 *            
+	 * @param hasSticker Whether the sticker should be displayed for this page
 	 */
-	public void addPage(String pageName){
+	public void addPage(String pageName, boolean hasSticker) {
+
+		//Add the page name
+		pageNames.add(pageName);
 		
-		pageNames.add(pageName) ;
+		//Create the page
+		KitPage page = new KitPage(pageName, childName, hasSticker);
+		
+		//Add to the card layout
+		this.add(page, pageName);
+		
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 
-		currentPage++ ;
-		
+		// Note that we're going to the next page
+		currentPage++;
+
+		// Show which page we are going to show
 		System.out.println(currentPage);
+
+		// Show the next page
 		CARD_LAYOUT.show(this, pageNames.get(currentPage));
 	}
-	
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 
+		// Create the frame
 		JFrame frame = new JFrame();
 
-		frame.getContentPane().add(new Passport());
+		// Add the passport to the frame--will need to figure out
+		// how to do the name getting part
+		frame.getContentPane().add(new Passport("Pretend child"));
 
+		// Set the size to the specified page size
 		frame.setSize(Passport.PAGE_WIDTH, Passport.PAGE_HEIGHT);
 
+		// Make visible
 		frame.setVisible(true);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
